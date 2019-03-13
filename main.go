7b51db16766790e5c48af66cccc318c7470c5962
Austin Stotts <austintstotts@gmail.com>
2019-03-13 16:11:07 -0500

    channels

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+// func main() {
+// 	fmt.Println("running... :)")
+
+// 	numbers := make(chan int, 10)
+
+// 	for i := 1; i <= 5; i++ {
+// 		numbers <- i
+// 	}
+
+// 	close(numbers)
+
+// 	for num := range numbers {
+// 		fmt.Println(num)
+// 	}
+
+// }
+
+func main() {
+
+}
