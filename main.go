4576007eb2534346196ca93aeed30e9d2b7babb7
Austin Stotts <austintstotts@gmail.com>
2019-03-13 17:24:59 -0500

    advanced selecting

--- main.go
+++ main.go
@@ -22,27 +22,58 @@ import (
 
 // }
 
+// *** *** *** *** *** *** *** ***
+
+// func main() {
+
+// 	numbers := make(chan int, 10)
+// 	stop := make(chan string, 1)
+
+// 	go func() {
+// 		for i := 1; i <= 10; i++ {
+// 			numbers <- i
+// 			time.Sleep(500 * time.Millisecond)
+// 		}
+// 		stop <- "stop"
+// 		close(numbers)
+// 		close(stop)
+// 	}()
+
+// 	for {
+// 		select {
+// 		case num := <-numbers:
+// 			fmt.Println(num)
+// 		case <-stop:
+// 			fmt.Println("STOP")
+// 			return
+// 		}
+// 	}
+// }
+
 func main() {
-
-	numbers := make(chan int, 10)
-	stop := make(chan string, 1)
-
+	odd := make(chan int, 10)
+	even := make(chan int, 10)
+	stop := make(chan int, 1)
+	results := make(chan int, 10)
 	go func() {
-		for i := 1; i <= 10; i++ {
-			numbers <- i
+		for i := 1; i <= 20; i++ {
+			if i%2 == 0 {
+				even <- i
+			} else {
+				odd <- i
+			}
 			time.Sleep(500 * time.Millisecond)
 		}
-		stop <- "stop"
-		close(numbers)
-		close(stop)
+		stop <- -1
 	}()
 
 	for {
 		select {
-		case num := <-numbers:
-			fmt.Println(num)
+		case e := <-even:
+			results <- (e + <-odd)
+			fmt.Println(<-results)
 		case <-stop:
-			fmt.Println("STOP")
+			fmt.Println("stop")
 			return
 		}
 	}
